be9976e13e1391efd576e74ba11a575f8a7d9c92
Felipe Nascimento <flpnascto@gmail.com>
2021-07-27 15:55:10 -0300

    requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if len(word) < 2:
+        return True
+    elif word[low_index] == word[high_index]:
+        return True and is_palindrome_recursive(
+            word[1:-1], 0, len(word[1:-1]) - 1
+        )
+    else:
+        return False
