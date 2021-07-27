1cbeedbed6b885b809046d30ab80acda97f1b855
Felipe Nascimento <flpnascto@gmail.com>
2021-07-27 14:47:05 -0300

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
-def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+def study_schedule(start_time, end_time, target_time):
+    if not target_time or not start_time or not end_time:
+        return 0
+    counter = 0
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time and end_time[i] >= target_time:
+            counter += 1
+    return counter
